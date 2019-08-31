1b7db1b995f704e7c1e7215a0948f63299344e88
skfdkfkalsrn <skfdkfkalsrn@naver.com>
2019-08-31 18:20:20 +0900

    ten0831

---
+++ cabbage/__init__.py
@@ -0,0 +1,6 @@
+from cabbage.model import CabbageModel
+
+
+if __name__ == '__main__':
+    m=CabbageModel()
+    m.create_model()
---
+++ members/controller.py
@@ -0,0 +1,18 @@
+from members.model import MemberModel
+
+class MemberController:
+    def __init__(self):
+        self.model=MemberModel()
+
+    def create_table(self):
+        self.model.create()
+        self.model.insert_many()
+        self.model.fetch_all()
+    def login(self,userid,password):
+        row=self.model.login(userid,password)
+        if row is None:
+            view='login.html'
+        else:
+            view='index.html'
+
+        return view
